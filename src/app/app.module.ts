23664cc7621ec47951be2238f4eabc77150cfce4
Jimmy419 <suifeng419@126.com>
2018-07-30 16:39:07 +0800

    frame set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { LeftNavigationComponent } from './left-navigation/left-navigation.component';
 
 @NgModule({
   imports: [
@@ -21,7 +22,8 @@ import { AppRoutingModule } from './app-routing.module';
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    LeftNavigationComponent
   ],
   bootstrap: [AppComponent]
 })
