476d428c696b6499043c4b3918008f8bd6a76555
Jimmy419 <suifeng419@126.com>
2018-07-31 13:09:35 +0800

    test

--- src/app/left-navigation/left-navigation.component.ts
+++ src/app/left-navigation/left-navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+// import { MENUS } from '../data/menus.json';
 
 @Component({
   selector: 'app-left-navigation',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-navigation.component.scss']
 })
 export class LeftNavigationComponent implements OnInit {
-
+  // menus: any[] = [];
+  // menus: Observable<string> = [];
   constructor() { }
 
   ngOnInit() {
+    // this.menus: Observable < any > = of('../data/menus.json');
+    // this.menus = of('../data/menus.json');
   }
 
 }
